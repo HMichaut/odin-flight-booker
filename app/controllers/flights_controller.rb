e4ed1490e35549592a33ae294b7a34c1cb0a895e
HMichaut <hectormichaut@gmail.com>
2021-07-30 00:25:51 +0200

    Search function finalized

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,14 +1,18 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all unless params.present?
-    @flights = Flight.where(start_airport: Airport.find_by(code: flight_params[:start_airport])).and(Flight.where(stop_airport: Airport.find_by(code: flight_params[:stop_airport]))) if params.present?
+    if params.present?
+      date_to_compare = Flight.find_by(id: flight_params[:start_datetime]).start_datetime.to_date
+      @flights = Flight.where(start_airport: Airport.find_by(id: flight_params[:start_airport])).and(Flight.where(stop_airport: Airport.find_by(id: flight_params[:stop_airport]))).and(Flight.where("DATE(start_datetime) = ?", date_to_compare))
+    else
+      @flights = Flight.all 
+    end
     @airport_options = Airport.all.map{ |u| [ u.code, u.id ]}
-    @date_options = Flight.all.map{ |u| [ u.start_datetime.strftime("%d/%m/%Y"), u.id ]}
+    @date_options = Flight.all.map{ |u| [ u.start_datetime.strftime("%d/%m/%Y"), u.id ]}.uniq { |x| x[0] }
   end
 
   private
 
   def flight_params
-    params.permit(:start_airport, :stop_airport, :passenger_number, :start_datetime)
+    params.permit(:start_airport, :stop_airport, :passenger_number, :start_datetime, :commit)
   end
 end
